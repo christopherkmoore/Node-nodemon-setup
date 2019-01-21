7b4f9328af7a1e178d57b952d5467a89ae53db71
christopherkmoore <christopherkmoore1@gmail.com>
2019-01-21 13:49:30 -0800

    adds router plus a route

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(req, res) { 
+        var responseJSON = { hello: "This is my api" };
+        res.json(responseJSON);
+    });
+
+app.use('/api', bookRouter);
+
 app.get('/', function(req, res) {
     res.send('Welcome to my API');
 });
